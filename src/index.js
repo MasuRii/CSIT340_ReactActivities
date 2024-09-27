85f788bbae01ed3e331c1face2b79bee59c13e2d
MasuRii マス リ <kanjiharigana@gmail.com>
2024-09-27 21:15:05 +0800

    Working finally code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // You can add global styles here
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Create a root.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Initial render
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
